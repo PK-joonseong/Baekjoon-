aeedc0d4bfaaccc7053ce403ffe5215be5e424f8
PK-joonseong <101553358+PK-joonseong@users.noreply.github.com>
2022-03-19 15:24:45 +0900

    Create BAEKJOON 18108.java

---
+++ BAEKJOON 18108.java
@@ -0,0 +1,12 @@
+// A + B = C
+// source code
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int year = input.nextInt();
+        System.out.println(year - (2541-1998));
+    }
+}   
