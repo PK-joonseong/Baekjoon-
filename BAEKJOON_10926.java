c2a97e80dcda60ad6707f8daf89c4570406aa7c6
PK-joonseong <101553358+PK-joonseong@users.noreply.github.com>
2022-03-19 14:45:58 +0900

    Create BAEKJOON_10926.java

---
+++ BAEKJOON_10926.java
@@ -0,0 +1,14 @@
+// 출력문 = 입력문 + ??!
+// source code
+import java.util.*; // Scanner Class 불러오기.
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("");
+        String line = input.nextLine();
+        System.out.print(line+ "??!");
+    }
+}
+
